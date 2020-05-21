88472b90da7e21029800c12b98771a191b11a1ca
akalaiev <akalaiev@leongaming.com>
2020-05-21 17:59:39 +0300

    vue create init app

---
+++ src/views/DefaultAppHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Message msg="Welcome to the Default App home page"/>
+  </div>
+</template>
+
+<script>
+import Message from '@/components/Message.vue';
+
+export default {
+  name: 'DefaultAppHome',
+  components: {
+    Message,
+  },
+};
+</script>
